697ebfc26ef75f7023d26a97abaf7c8d040201d6
Gustavo0033 <gu2003.mendonca48@gmail.com>
2022-05-04 11:48:57 -0300

    Projeto Aplicativo PlayStation

---
+++ app/src/main/java/com/example/playstationapp/MainActivity.java
@@ -0,0 +1,34 @@
+package com.example.playstationapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    private View btnSessao;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btnSessao = findViewById(R.id.btnSessao);
+
+        btnSessao.setOnClickListener(View -> {
+            Intent intent = new Intent(MainActivity.this, TelaIDsessao.class);
+            startActivity(intent);
+        });
+
+
+    }
+
+
+
+
+
+
+}
---
+++ app/src/main/java/com/example/playstationapp/PrepareJogar.java
@@ -0,0 +1,14 @@
+package com.example.playstationapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class PrepareJogar extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_prepare_jogar);
+    }
+}
