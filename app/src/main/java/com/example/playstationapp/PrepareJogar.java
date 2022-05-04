8928fd01265804ceb184f3bba32ec6911aeab72a
Gustavo0033 <gu2003.mendonca48@gmail.com>
2022-05-04 15:55:05 -0300

    Projeto Aplicativo PlayStation1

--- app/src/main/java/com/example/playstationapp/PrepareJogar.java
+++ app/src/main/java/com/example/playstationapp/PrepareJogar.java
@@ -2,13 +2,26 @@ package com.example.playstationapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class PrepareJogar extends AppCompatActivity {
+
+    private View btn_seguinte02;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_prepare_jogar);
-    }
+
+        btn_seguinte02 = findViewById(R.id.btn_seguinte02);
+
+        btn_seguinte02.setOnClickListener(View -> {
+            Intent intent = new Intent(PrepareJogar.this, ContinueConversa.class);
+            startActivity(intent);
+        });
+
+
+}
 }
--- app/src/main/java/com/example/playstationapp/TelaIDsessao.java
+++ app/src/main/java/com/example/playstationapp/TelaIDsessao.java
@@ -2,13 +2,25 @@ package com.example.playstationapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class TelaIDsessao extends AppCompatActivity {
+
+    private View btn_seguinte;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tela_idsessao);
-    }
+
+        btn_seguinte = findViewById(R.id.btn_seguinte);
+
+        btn_seguinte.setOnClickListener(View -> {
+            Intent intent = new Intent(TelaIDsessao.this, PrepareJogar.class);
+            startActivity(intent);
+    });
+}
 }
